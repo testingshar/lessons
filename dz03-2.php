fabf8d29f5a14d29f06791c4221412c7f01a08c3
testingshar <testshar@yandex.ru>
2015-07-15 05:21:44 +0400

    выполнил все задания

---
+++ dz03-2.php
@@ -0,0 +1,22 @@
+<?php
+//ЗАДАНИЕ 2⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅⋅
+
+function randArr()
+{
+	$min = 1;
+	$max = 7;
+	$rnd = rand(1,10);
+	$ms = array(rand($min, $max));
+	$mass = array_fill($min, $rnd, $ms);
+	echo 'Просто рандомный массив ' . "<br>";
+	print_r($mass);
+	echo "<br><br>" . "Массив отсортированный в обратном порядке по ключам через ф-ию krsort <br>";
+	krsort($mass, SORT_NUMERIC);
+	foreach ($mass as $key => $value) {
+		print_r($key);
+		echo ' ';
+	}
+}
+echo randArr();
+
+?>
