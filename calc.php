8e0f07b85a7474acac0c01f2eba3fee58431da17
testingshar <testshar@yandex.ru>
2015-07-21 03:30:44 +0400

    поправил код

--- calc.php
+++ calc.php
@@ -2,18 +2,16 @@
 
 $firstnumber = $_POST['firstnumber'];
 $secondnumber = $_POST['secondnumber'];
-if (empty($_POST['firstnumber']) && empty($_POST['secondnumber'])) {
+if (empty($firstnumber) && empty($secondnumber)) {
     echo 'По ходу кто то что то да где то и когда то забыл во что то вписать ...';
-} elseif($_POST['operations'] == 'plus') {
+} elseif ($firstnumber == 0 || $secondnumber == 0) {
+	echo 'Ни одно из чисел не должно быть равным 0, математически это не совсем правильно';
+} elseif ($_POST['operations'] == 'plus') {
     echo $firstnumber + $secondnumber;
 } elseif ($_POST['operations'] == 'minus') {
     echo $firstnumber - $secondnumber;
 } elseif ($_POST['operations'] == 'multiply') {
     echo $firstnumber * $secondnumber;
 } elseif ($_POST['operations'] == 'division') {
-    if($secondnumber == 0) {
-        echo 'Ты поделил на 0 - за это я отрублю тебе руку которой ты посмел нажать клавишу деления ))))';
-    } else {
-        echo $firstnumber / $secondnumber;
-    }
+    echo $firstnumber / $secondnumber;
 }
