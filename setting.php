44af93415b36a30613f4fb51a668dce68d7f4dc4
testingshar <testshar@yandex.ru>
2015-08-28 01:29:37 +0400

    задание под *

---
+++ setting.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Файл настроек</title>
+</head>
+<body>
+<h1>Выбирете  стиль оформления</h1>
+<label>
+<input type="radio" name="butt01" value="st01">Стиль оформления 1</input>
+</label>
+<br>
+
+<label>
+<input type="radio" name="butt02" value="st02">Стиль оформления 2</input>
+</label>
+<br>
+
+<label>
+<input type="radio" name="butt03"  value="st03">Стиль оформления 3</input>
+</label>
+</body>
+</html>
